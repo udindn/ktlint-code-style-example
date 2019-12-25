05e44758c9461989d788782b4eba83024a33bf22
DWI NGAFIFUDIN <DwiNgafi@aksaramaya.com>
2019-12-25 11:13:27 +0700

    disable wildcard

--- src/androidTest/java/com/example/ktlintcodestyleexample/ExampleInstrumentedTest.kt
+++ src/androidTest/java/com/example/ktlintcodestyleexample/ExampleInstrumentedTest.kt
@@ -2,7 +2,7 @@ package com.example.ktlintcodestyleexample
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
+import org.junit.Assert.* // ktlint-disable
 import org.junit.Test
 import org.junit.runner.RunWith
 
